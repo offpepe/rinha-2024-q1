6ccf24197bd72c43e9c764c2dabb4c31cfef33df
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2024-03-02 20:36:37 -0300

    Revert "Merge branch 'feat/virtualization'"
    This reverts commit 8967e3f10af66c86aad1f8120e9dd9795517311a, reversing
    changes made to ab8c01f2d8714c67d1621fad25fda91cc0189fbf.

--- Extensions/ControllerSetup.cs
+++ Extensions/ControllerSetup.cs
@@ -7,20 +7,19 @@ public static class ControllerSetup
     public static void SetControllers(this WebApplication app)
     {
         app.MapGet("/clientes/{id:int}/extrato",
-            async (int id, [FromServices] VirtualService vdb) =>
+            async (int id, [FromServices] Database db) =>
             {
-                if (id is < 1 or > 5) return Results.NotFound();
-                var (client, transactions) = await vdb.GetClient(id);
-                return Results.Ok(new ExtractDto(new SaldoDto(client[0], client[1]), transactions));
+                var balance = await db.GetExtract(id);
+                return !balance.HasValue ? Results.NotFound() : Results.Ok(balance.Value);
             });
         app.MapPost("/clientes/{id:int}/transacoes", async (int id,
-            [FromServices] VirtualService vdb,
+            [FromServices] Database db,
             [FromBody] CreateTransactionDto dto) =>
         {
-            if (id is < 1 or > 5) return Results.NotFound();
             if (!ValidateTransaction(dto)) return Results.UnprocessableEntity();
-            var result = await vdb.DoTransaction(id, dto.Tipo, dto.Valor, dto.Descricao);
-            return result[1] == -1 ? Results.UnprocessableEntity() : Results.Ok(new ValidateTransactionDto(result[1], result[0]));
+            var values = await db.DoTransaction(id, dto);
+            if (values == null) return Results.NotFound();
+            return values[1] < 0 ? Results.UnprocessableEntity() : Results.Ok(new ValidateTransactionDto(values[1], values[0]));
         });
     }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Npgsql;
 using Rinha2024.Dotnet;
 using Rinha2024.Dotnet.Extensions;
 
@@ -6,18 +7,20 @@ var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.WebHost.UseKestrel(opt =>
 {
-    opt.Limits.MaxConcurrentConnections = int.TryParse(Environment.GetEnvironmentVariable("MAX_CONCURRENT_CONNECTIONS"), out var maxConcurrentConnections) ? maxConcurrentConnections : 3000;
+    opt.Limits.MaxConcurrentConnections = int.TryParse(Environment.GetEnvironmentVariable("MAX_CONCURRENT_CONNECTIONS"), out var maxConcurrentConnections) ? maxConcurrentConnections : 500;
     opt.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(60);
 });
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
-builder.Services.AddSingleton<VirtualService>();
+builder.Services.AddSingleton<NpgsqlDataSource>(_ => new NpgsqlSlimDataSourceBuilder(builder.Configuration.GetConnectionString("DB")!).Build());
+builder.Services.AddSingleton<Database>();
 builder.Services.AddSingleton<ExceptionMiddleware>();
-builder.Services.AddLogging(l => l.AddSimpleConsole());
+builder.Services.AddLogging(l => l.AddSimpleConsole()); 
 var app = builder.Build();
-app.SetControllers();   
+app.SetControllers();
+await app.Services.GetRequiredService<Database>().Stretching();
 app.Run();
 
 [JsonSerializable(typeof(SaldoDto))]
